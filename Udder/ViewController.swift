6b3412ecb23c54721593fd2ee952e27d395154a8
Vincent <vincentbaldwin@comcast.net>
2019-09-22 18:10:27 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Vincent Baldwin on 9/22/19.
+//  Copyright © 2019 Vincent Baldwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
